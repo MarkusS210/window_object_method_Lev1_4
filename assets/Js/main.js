9abf3a660ca9499c0cca7d25988ca7cd39c328df
Markus <Markus.Schneiderm3@googlemail.com>
2023-01-03 15:25:32 +0100

    window_object_method_Lev1_4

---
+++ assets/Js/main.js
@@ -0,0 +1,36 @@
+//window.alert("Hallo Welt"); // Text mit Popup
+
+//window.prompt("Bitte geben Sie Ihren Namen ein.");
+// Popup - Text mit Eingabesfeld
+
+//window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+//Popup - Text mit Eingabesfeld + Abbrechen u. Ok Feld
+
+
+//window.confirm("Stimmen Sie meiner Meinung zu?"); 
+//Popup - Text mit Abbrechen u. Ok Feld
+
+
+/*let alter = prompt( "Gib mir bitte dein Alter:" );
+console.log(alter);*/
+// Variable + prompt = Altersabfrage mit Eingabefeld
+
+
+/*let b = 5;
+let a = b * 5 -3;
+alert(a);*/
+//Variable b ist 5 / Variable a ist 22 / alert ist das Warnfeld
+
+
+/*let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+let grosse = "2,72m";
+alert(mann + grosse);*/
+/* Im Warnfeld wird mit die Variable mann zuerst angezeigt, danach die Variable grosse */
+//Robert Wadlow ist der größte Mann der Welt: 2,72m
+
+
+
+
+
+
+
